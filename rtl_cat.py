1d9a06f33618b801cf844e93fc776956a2e0e33f
KeyLo99 <keylo99official@gmail.com>
2019-01-31 04:02:03 +0300

    Import pyrtlsdr module

--- rtl_cat.py
+++ rtl_cat.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+def import_rtlsdr():
+    try:
+        global RtlSdr
+        from rtlsdr import RtlSdr
+    except ImportError as e:
+        print("pyrtlsdr not found.")
+
 def main():
-    pass
+    import_rtlsdr()
+    rtl_sdr = RtlSdr()
 
 if __name__ == "__main__":
     main()
