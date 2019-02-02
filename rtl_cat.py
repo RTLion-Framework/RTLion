3e1aab1d1ae29ca93314e8517d2253cc34825905
KeyLo99 <keylo99official@gmail.com>
2019-02-03 02:55:08 +0300

    Continously create graph from samples [test]

--- rtl_cat.py
+++ rtl_cat.py
@@ -8,16 +8,16 @@ from pylab import *
 def main():
 
     rtl_sdr = RTLSdr(**parse_cli_args())
-    samples = rtl_sdr.read_samples()
-    rtl_sdr.close()
 
-    def plot_samples(samples, RTLSdr):
+    for i in range(50):
+        samples = rtl_sdr.read_samples()
         psd(samples, NFFT=1024, Fs=int(rtl_sdr.sample_rate)/1e6, Fc=int(rtl_sdr.center_freq)/1e6)
         xlabel('Frequency (MHz)')
         ylabel('Relative power (dB)')
-        show()
+        pause(0.005)
+        clf()
 
-    plot_samples(samples, rtl_sdr)
+    rtl_sdr.close()
 
 if __name__ == "__main__":
     main()
