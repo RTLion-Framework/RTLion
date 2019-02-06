a1b5632f3d56211a6ca4f9d723f309b1a4038741
KeyLo99 <keylo99official@gmail.com>
2019-02-07 02:49:09 +0300

    Flask test code added

--- rtl_cat.py
+++ rtl_cat.py
@@ -3,13 +3,22 @@
 
 from rtlcat.rtldev import RTLSdr
 from rtlcat.argparser import parse_cli_args
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "~"
 
 
 def main():
 
-    rtl_sdr = RTLSdr(**parse_cli_args())
-    rtl_sdr.create_graph(True, 100)
-    rtl_sdr.close()
+    app.run(host='0.0.0.0', port=5000)
 
+    #rtl_sdr = RTLSdr(**parse_cli_args())
+    #rtl_sdr.create_graph(True, 100)
+    #rtl_sdr.close()
+1
 if __name__ == "__main__":
     main()
