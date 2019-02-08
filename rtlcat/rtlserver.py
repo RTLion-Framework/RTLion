0f21c0177a90661d77b6e2637b9fe7cdd62d38f9
KeyLo99 <keylo99official@gmail.com>
2019-02-09 02:47:17 +0300

    Flask initializer function added

--- rtlcat/rtlserver.py
+++ rtlcat/rtlserver.py
@@ -7,6 +7,7 @@ class FlaskServer:
     def __init__(self, server_host='0.0.0.0', server_port=8081):
         self.server_addr = str(server_host) + ":" + str(server_port)
         self.import_flask()
+        self.initialize_flask()
 
     def import_flask(self):
         try:
@@ -15,4 +16,12 @@ class FlaskServer:
         except:
             print("Flask framework not found.")
             sys.exit()
+
+    def initialize_flask(self):
+        try:
+            self.flask_server = Flask(__name__)
+        except:
+            print("Could not initialize Flask server.")
+            sys.exit()
+
 
