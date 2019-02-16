9d0dc3840e93cf9505c3f2be64ab3cc27d6a52e7
KeyLo99 <keylo99official@gmail.com>
2019-02-16 03:35:29 +0300

    Show arguments on settings page

--- rtlcat/rtldev.py
+++ rtlcat/rtldev.py
@@ -10,12 +10,16 @@ class RTLSdr:
         self.default_device_id = 0
         self.default_sample_rate = 2.048e6
         self.default_gain = 'auto'
-        self.args = args
         self.dev_id = args['dev'] if args['dev'] else self.default_device_id
         self.sample_rate = args['samprate'] if args['samprate'] \
                                             else self.default_sample_rate
         self.gain = int(args['gain']) if args['gain'] else self.default_gain
         self.center_freq = args['freq']
+        self.args = {
+            'freq': self.center_freq, 
+            'samprate': self.sample_rate, 
+            'dev': self.dev_id, 
+            'gain': self.gain}
         self.dev = None
     
     def import_rtlsdr(self):
