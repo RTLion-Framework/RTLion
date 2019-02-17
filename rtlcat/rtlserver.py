1d3180655444e772a6a5702cc5399c3809ac849b
KeyLo99 <keylo99official@gmail.com>
2019-02-17 21:55:07 +0300

    Stop reading feature & removed some unused declarations

--- rtlcat/rtlserver.py
+++ rtlcat/rtlserver.py
@@ -38,10 +38,6 @@ class FlaskServer:
             def page_graph(): return render_template('graph.html', async_mode=self.socketio.async_mode)
 
             def ping_pong(): emit('server_pong')
-            def send_args_index(): self.socketio.emit('client_message', self.rtl_sdr.args, \
-                                namespace=self.index_namespace)
-            def send_args_settings(): self.socketio.emit('client_message', self.rtl_sdr.args, \
-                                namespace=self.settings_namespace)
             def send_args_graph(): self.socketio.emit('cli_args', self.rtl_sdr.args, \
                                 namespace=self.graph_namespace)
 
@@ -49,13 +45,13 @@ class FlaskServer:
             self.socketio = SocketIO(self.flask_server, async_mode=None)
     
             self.flask_server.route(self.index_namespace)(page_index)
-            self.socketio.on('send_args', namespace=self.index_namespace)(send_args_index)
 
             self.flask_server.route(self.graph_namespace, methods=['GET', 'POST'])(page_graph)
             self.socketio.on('disconnect_request', namespace=self.graph_namespace)(self.disconnect_request)
             self.socketio.on('create_fft_graph', namespace=self.graph_namespace)(self.create_fft_graph)
             self.socketio.on('send_cli_args', namespace=self.graph_namespace)(send_args_graph)
             self.socketio.on('update_settings', namespace=self.graph_namespace)(self.update_settings)
+            self.socketio.on('stop_sdr', namespace=self.graph_namespace)(self.stop_sdr)
             self.socketio.on('server_ping', namespace=self.graph_namespace)(ping_pong)
             
         except Exception as e:
@@ -74,18 +70,22 @@ class FlaskServer:
     def disconnect_request(self):
         self.socketio.stop()
 
+    def stop_sdr(self):
+        self.rtl_sdr.close()
+        print('x')
+
     def create_fft_graph(self):
-        self.send_to_server("Creating FFT graph from samples... [>]")
+        self.socketio.emit(
+            'client_message', 
+            {'data': 'Creating FFT graph from samples...'}, 
+            namespace=self.graph_namespace)
         with self.thread_lock:
             if self.thread is None:
                 self.c_read = True
                 self.thread = self.socketio.start_background_task(self.rtlsdr_thread)
 
-    def send_to_server(self, msg, count=0):
-        self.socketio.emit(
-            'client_message', 
-            {'data': msg, 'count': count}, 
-            namespace=self.graph_namespace)
+    def update_settings(self, args):
+        self.rtl_sdr.set_args(args)
 
     def rtlsdr_thread(self):
         self.rtl_sdr.init_device()
@@ -97,6 +97,4 @@ class FlaskServer:
             namespace=self.graph_namespace)
             self.socketio.sleep(0.4)
 
-    def update_settings(self, args):
-        self.rtl_sdr.set_args(args)
 
