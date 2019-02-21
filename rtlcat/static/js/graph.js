e32ab90b73365a946fcac646db5f5b5b6ef4775e
KeyLo99 <keylo99official@gmail.com>
2019-02-21 14:32:16 +0300

    HTML/Js refactor I

--- rtlcat/static/js/graph.js
+++ rtlcat/static/js/graph.js
@@ -1,9 +1,19 @@
 $(document).ready(graphSocket);
+
+var graph_namespace = '/graph';
+var create_graph = true;
+var ping_pong_times = [];
+var start_time;
+var read_count;
+var socket;
+
+function pageInit(){
+    $('#colFFTGraph').hide();
+}
+
 function graphSocket() {
-    $('#fft_graph_column').hide();
-    graph_namespace = '/graph';
-    create_graph = true;
-    var socket = io.connect(location.protocol + '//' + document.domain + 
+    pageInit();
+    socket = io.connect(location.protocol + '//' + document.domain + 
                  ':' + location.port + graph_namespace);
 
     socket.on('connect', function() {
@@ -11,66 +21,37 @@ function graphSocket() {
     });
 
     socket.on('client_message', function(msg) {
-        $('#log').append(msg.data + '<br>');
-        $('#log').scrollTop($('#log').height());
+        $('#divLog').append(msg.data + '<br>');
+        $('#divLog').scrollTop($('#divLog').height());
     });
-    $('form#create_graph').submit(function(event) {
-        if (create_graph){
-            // Check arguments
-            create_graph = false;
-            $('#btn_graph').val("Stop");
-            read_count = 0;
-            socket.emit('create_fft_graph');
-        }else{
-            create_graph = true;
-            $('#btn_graph').val("Create FFT graph");
-            socket.emit('stop_sdr');
-        }
-        return false;
-    });
-
     socket.on('fft_data', function(msg) {
-        $('#fft_graph').attr("src", "data:image/png;base64," + msg.data);
+        $('#imgFFTGraph').attr("src", "data:image/png;base64," + msg.data);
         read_count++;
-        if($('#n_read').val() == "-1"){
-            $('#read_count').text('(' + read_count + '/∞)');
+        if($('#inpNumRead').val() == "-1"){
+            $('#spnReads').text('(' + read_count + '/∞)');
         }else{
-            $('#read_count').text('('+ read_count+ '/' + $('#n_read').val() + ')');
+            $('#spnReads').text('('+ read_count+ '/' + $('#inpNumRead').val() + ')');
         }
-        if(!$('#fft_graph_column').is(':visible')){
-            $('#fft_graph_column').show();
+        if(!$('#colFFTGraph').is(':visible')){
+            $('#colFFTGraph').show();
         }
     });
-
     socket.on('cli_args', function(cliargs) {
         args = cliargs.args;
-        $("#dev_index").val(args.dev);
-        $("#samp_rate").val(args.samprate);
-        $("#dev_gain").val(args.gain);
-        $("#center_freq").val(args.freq);
-        $("#n_read").val(args.n)
-        $("#interval").val(args.i);
+        $("#inpDevIndex").val(args.dev);
+        $("#inpSampRate").val(args.samprate);
+        $("#inpDevGain").val(args.gain);
+        $("#inpCenterFreq").val(args.freq);
+        $("#inpNumRead").val(args.n)
+        $("#inpInterval").val(args.i);
         read_count = args.n;
         if (cliargs.status == 1){
-            $('#settings_log').text('Settings saved.');
+            $('#spnSettingsLog').text('Settings saved.');
             setTimeout(function() {
-                $('#settings_log').text('');
+                $('#spnSettingsLog').text('');
             }, 1000);
         }
     });
-    $('#save_settings *').filter(':input').change(function() {
-        // Check arguments here
-        socket.emit('update_settings',
-        {'dev': parseInt($('#dev_index').val()), 
-        'samprate': parseInt($('#samp_rate').val()), 
-        'gain': $('#dev_gain').val(), 
-        'freq': parseInt($('#center_freq').val()),
-        'n': parseInt($('#n_read').val()),
-        'i': parseInt($('#interval').val())});
-    });
-    var ping_pong_times = [];
-    var start_time;
-    var read_count;
     window.setInterval(function() {
         start_time = (new Date).getTime();
         socket.emit('server_ping');
@@ -82,7 +63,31 @@ function graphSocket() {
         var sum = 0;
         for (var i = 0; i < ping_pong_times.length; i++)
             sum += ping_pong_times[i];
-        $('#ping_pong').text(Math.round(10 * sum / ping_pong_times.length) / 10 + "ms");
+        $('#spnPingPongs').text(Math.round(10 * sum / ping_pong_times.length) / 10 + "ms");
     });
 
+    $('form#formCreateGraph').submit(function(event) {
+        if (create_graph){
+            // Check arguments
+            create_graph = false;
+            $('#btnCreateGraph').val("Stop");
+            read_count = 0;
+            socket.emit('create_fft_graph');
+        }else{
+            create_graph = true;
+            $('#btnCreateGraph').val("Create FFT graph");
+            socket.emit('stop_sdr');
+        }
+        return false;
+    });
+    $('#formSaveSettings *').filter(':input').change(function() {
+        // Check arguments here
+        socket.emit('update_settings',
+        {'dev': parseInt($('#inpDevIndex').val()), 
+        'samprate': parseInt($('#inpSampRate').val()), 
+        'gain': $('#inpDevGain').val(), 
+        'freq': parseInt($('#inpCenterFreq').val()),
+        'n': parseInt($('#inpNumRead').val()),
+        'i': parseInt($('#inpInterval').val())});
+    });
 }
