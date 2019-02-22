eeb74f638848dcc4d2a1bb8cd74b9247f48dd90d
KeyLo99 <keylo99official@gmail.com>
2019-02-22 21:39:15 +0300

    Add starting message to main file

--- rtl_cat.py
+++ rtl_cat.py
@@ -4,9 +4,14 @@
 from rtlcat.rtlserver import FlaskServer
 from rtlcat.rtldev import RTLSdr
 from rtlcat.argparser import parse_cli_args
+from rtlcat.logcl import LogCL
+
+def start_message():
+    print("rtl_cat project")
+    LogCL().log("Starting...")
 
 def main():
-    print("rtl_cat project")
+    start_message()
     cli_args = parse_cli_args()
     rtl_sdr = RTLSdr(**cli_args)
     flask_server = FlaskServer(rtl_sdr, cli_args['host:port'].split(':'))
