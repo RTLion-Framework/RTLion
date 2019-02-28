78f3733da7fc64a502b368c8ad681dcf1a2c6f44
KeyLo99 <keylo99official@gmail.com>
2019-03-01 00:38:44 +0300

    Move js script in index.html to index.js

---
+++ scripts/static/js/index.js
@@ -0,0 +1,18 @@
+function fadeInAnim(){
+    $("#imgLogo").delay(50).animate({"opacity": "1"}, 700);
+    $("#spnDesc").delay(500).animate({"opacity": "1"}, 700);
+    $("#divFFTGraphArea").delay(800).animate({"opacity": "1"}, 700);
+}
+$(document).ready(function() {
+    fadeInAnim();
+    $("#btnFFTGraph").click(function () {
+        window.location.replace("/graph"); 
+    });
+    namespace = '/';
+    var socket = io.connect(location.protocol + '//' + document.domain + 
+                 ':' + location.port + namespace);
+
+    socket.on('connect', function() {
+        //
+    });
+});
