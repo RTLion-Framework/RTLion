388ee601752e4a341ec749e7a2195ee68707ca8e
KeyLo99 <keylo99official@gmail.com>
2019-03-19 16:01:17 +0300

    Derive SocketIO functions in graph.js (to app.js)

--- scripts/static/js/app.js
+++ scripts/static/js/app.js
@@ -7,7 +7,24 @@ function appPageInit(){
     socket = io.connect(location.protocol + '//' + document.domain + 
                  ':' + location.port + app_namespace);
     
-    
+    socket.on('connect', function() {
+        socket.emit('send_cli_args');
+    });
+
+    socket.on('cli_args', function(cliargs) {
+        args = cliargs.args;
+        for (var i in args){
+            if (i != 'freq')
+                args[i] = args[i] || 0;
+        }
+        alert(args);
+        /*$("#inpDevIndex").val(args.dev);
+        $("#inpSampRate").val(args.samprate);
+        $("#inpDevGain").val(args.gain);
+        $("#inpCenterFreq").val(args.freq);
+        $("#inpNumRead").val(args.n);
+        $("#inpInterval").val(args.i);*/
+    });
 }
 
 var clientJS;
