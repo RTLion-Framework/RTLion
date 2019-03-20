9e232ebe89418604d91df0a9c29b3eddb9851b27
KeyLo99 <keylo99official@gmail.com>
2019-03-20 21:25:52 +0300

    Create checkArgs(args) argument in app.js

--- scripts/static/js/app.js
+++ scripts/static/js/app.js
@@ -48,7 +48,14 @@ function getCliArgs(){
     socket.emit('send_cli_args');
     return JSON.stringify(rtlArgs, null, 2);
 }
-
+function checkArgs(args){
+    if (args['dev'] < 0 || args['dev'] > 20 || args['samprate'] < 0 ||
+     args['gain'] < 0 || args['freq'] <= 0 || args['freq'] == "" || 
+     isNaN(args['freq']) || args['freq'] == null || args['i'] < 0 || args['n'] < -1){
+        return false;
+    }
+    return true;
+}
 function setCliArgs(newArgs){
-    
+
 }
