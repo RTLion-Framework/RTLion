e717d913bbf49ee590fbe5dd7073560f68962b9b
KeyLo99 <keylo99official@gmail.com>
2019-04-05 23:55:23 +0300

    Connect to socket via scan_namespace

--- scripts/static/js/scan.js
+++ scripts/static/js/scan.js
@@ -1,10 +1,11 @@
 $(document).ready(scannerSocket);
 
-var graph_namespace = '/scan';
+var scan_namespace = '/scan';
 var ping_pong_times = [];
 var start_time;
 var socket;
 
 function scannerSocket(){
-    
+    socket = io.connect(location.protocol + '//' + document.domain + 
+                 ':' + location.port + scan_namespace);
 }
