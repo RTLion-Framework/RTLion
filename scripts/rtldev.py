e4bb17f07ad6b3083d2b7938af1d9499fa446718
KeyLo99 <keylo99official@gmail.com>
2019-04-08 17:45:46 +0300

    Change sorted array name

--- scripts/rtldev.py
+++ scripts/rtldev.py
@@ -90,7 +90,8 @@ class RTLSdr:
     def find_peaks(self, Y, F, n=3):
         try:
             import numpy as np
-            sorted_Y = Y[np.argsort(Y)[-n:]]
+            Y_sorted = Y[np.argsort(Y)[-n:]]
+            
         except:
             self.logcl.log("Failed to find peaks on graph.\n" + str(e), 'error')
 
